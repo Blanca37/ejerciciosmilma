a9318fb7d90e54883c5b228531a32cec68a5ceb4
Blanca37 <61311852+Blanca37@users.noreply.github.com>
2020-02-23 18:50:49 +0100

    Add files via upload
    Menú fijo Javascript. Fecha 12/12/2019

---
+++ menufijo.js
@@ -0,0 +1,19 @@
+jQuery('document').ready(function($){
+
+var menuBtn = $('.menu-icon');//declaro las variables//
+var menu = $('.navigation ul');
+
+menuBtn.click(function(){
+
+if (menu.hasClass('show')){
+
+    menu.addClass('show');
+    
+}else{
+    menu.addClass('show');
+
+}
+
+});
+
+});
